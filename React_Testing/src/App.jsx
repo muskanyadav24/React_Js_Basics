e3da1417185fc262f610eba52d9e97c81bc54464
muskanyadav24 <muskanyadav92163@gmail.com>
2026-02-24 18:13:08 +0530

    States_Effects

--- React_Testing/src/App.jsx
+++ React_Testing/src/App.jsx
@@ -1,13 +1,42 @@
+import { useState , useEffect} from 'react'
 import Header from './components/Header/Header'
 import Test from './components/Test/Test'
 import Demo from './components/Demo/Demo'
 import './App.css'
 
 function App() {
+
+  // let name = "Miku";
+
+  const [name, setName ] = useState("Miku");
+
+  const handlNameChange = () => {
+    setName("muku24");
+  }
+
+  useEffect(() => {
+    console.log("Component all render");
+  });
+
+  useEffect(() => {
+    console.log("Component mounted");
+  }, []);
+
+  useEffect(() => {
+    console.log("Component updated");
+  }, [name]);
+
+  useEffect(() => {
+    console.log("Component unmounted");
+    return () => {
+      console.log("Component unmounted");
+    }
+  }, []);
+
   return (
     <>
       <Header />
-      <Test color="red"/>
+      <Test color="red" name={name} handlNameChange={handlNameChange}/>
       <Demo />
     </>
   )
--- React_Testing/src/components/Test/Test.jsx
+++ React_Testing/src/components/Test/Test.jsx
@@ -2,6 +2,10 @@ const Test = (props) => {
     return (
         <div>
             <h1 style={{color:props.color}}>Welcome to React Testing</h1>
+
+            
+            <h3>Welcome {props.name}</h3>
+            <button onClick={props.handlNameChange}>Change Name</button>
         </div>
     );
 };
