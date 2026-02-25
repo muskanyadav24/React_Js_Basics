5b0caef1aeb40c30deadd2a080e117a5c53f1f8a
muskanyadav24 <muskanyadav92163@gmail.com>
2026-02-25 18:10:26 +0530

    Class&Function

--- React_Testing/src/components/Demo/Demo.jsx
+++ React_Testing/src/components/Demo/Demo.jsx
@@ -1,9 +1,51 @@
-const Demo = () => {
-    return (
-        <div>
-            <p>Hello World</p>
-        </div>
-    );
-};
+// // Functional Component
+
+// const Demo = () => {
+//     return (
+//         <div>
+//             <p>Hello World</p>
+//         </div>
+//     );
+// };
+
+// export default Demo;
+
+
+
+// Class Component
+import React from "react";
+
+class Demo extends React.Component{
+
+    // constructor(props){
+    //     super(props);
+    //     this.state = {
+    //         name: "Miku",
+    //         age: 20
+    //     }
+    // }
+
+    // Built-in method
+    componentDidMount(){
+        console.log("Component mounted");
+    }
+
+    componentDidUpdate(){
+        console.log("Component updated");
+    }
+
+    componentWillUnmount(){
+        console.log("Component unmounted");
+    }
+
+    render(){
+        return(
+            <div>
+                
+                <h1>Class Component</h1>
+            </div>
+        )
+    }
+}
 
 export default Demo;
